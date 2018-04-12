4f76d9408ff87a4bf41cd27bddfa3fae2efc0fd6
powerful23 <wenzhuo235@gmail.com>
2018-04-12 16:01:59 -0700

    reorganize logger

--- src/LoggerProvider.ts
+++ src/LoggerProvider.ts
@@ -9,25 +9,19 @@ export default class LoggerProvider {
         return 'Analytics';
     }
 
+    public getProviderName() {
+        return 'provider';
+    }
+
     public configure(config) {
-  
         const conf = config? config : {};
         this._config = Object.assign({}, this._config, conf);
         return this._config;
     }
 
-    public async startSession(config) {
-        console.log('start Session from Logger provider');
-        return true;
-    }
-
-    public async stopSession(config) {
-        console.log('stop Session from Logger provider');
-        return true;
-    }
-
-    public async record(params, config) {
-        console.log('record from Logger provider');
+    public async record(params): Promise<boolean> {
+        console.log('record from Logger provider with params', params);
+        // if success
         return true;
     }
 }
